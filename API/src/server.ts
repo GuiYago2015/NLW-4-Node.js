4168321910a0e8ab98ebc0528522a03b1d6134a8
Guilherme Yago <guiyago2015@gmail.com>
2021-02-25 02:08:31 -0300

    aula 2

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,25 +1,11 @@
+import 'reflect-metadata';
 import express, { response } from 'express';
+import "./database";
+import {router} from "./routes";
 
 const app = express();
 
-/*
-GET = Busca
-POST = Salvar 
-PUT = Alterar 
-DELETE = deletar
-PATCH = Alteração específica
-*/
-
-// http://localhost:3333/users
-app.get("/", (request, response) => {
-    return response.json({message: "Hello World"});
-})
-
-// 1 param > Rota(Recurso API)
-// 2 param >request, response
-app.post("/", (request, response) => {
-    // recebeu os dados para salvar 
-    return response.json({message: "os dados foram salvos com sucesso!"});
-})
+app.use(express.json());
+app.use( router );
 
 app.listen(3333, () => console.log("Server is running..."));
