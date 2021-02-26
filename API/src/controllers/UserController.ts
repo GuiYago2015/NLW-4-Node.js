9c1a1ba71b5b5b659b5bdfe81a8f978acd4ea713
Guilherme Yago <guiyago2015@gmail.com>
2021-02-26 00:47:35 -0300

    aula 3

--- API/src/controllers/UserController.ts
+++ API/src/controllers/UserController.ts
@@ -1,12 +1,12 @@
-import {Request, Response} from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/user';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 
 class UserController{
     async create(request: Request, response: Response){
         const {name, email} = request.body;
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
         
         
         const userAlreadyExists = await usersRepository.findOne({
@@ -25,9 +25,9 @@ class UserController{
 
         await usersRepository.save(user);
 
-        return response.json(user);
+        return response.status(201).json(user);
     }
 }
 
 
-export {  UserController }
+export { UserController };
